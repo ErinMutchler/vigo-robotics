773faa851435541159691905a99452e4793875cf
Kkilker <kkilker@vigo.lib.in.us>
2022-12-13 16:52:40 -0500

    Starting to code login page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,28 @@
 <template>
-
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Login</title>
+    </head>
+    <body>
+      <div class="block1">
+      <div class="bounce">
+      <img src="https://i.ibb.co/SJFdSg2/html-css-roboi.png">
+      </div>
+    </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  
+    </body>
+  </html>
 </template>
 
 <script>
@@ -9,5 +32,18 @@ export default {
 </script>
 
 <style scoped>
+.block1 {
+background: none;
+height: 100%;
+width: 40%;
+text-align: center;
+}
 
+.bounce {
+display: flex;
+text-align: center;
+justify-content: center;
+position: relative;
+top: 600px;
+}
 </style>
