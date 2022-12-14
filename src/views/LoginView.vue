5c9c67dde8b1660eee42a9e5db62f0831a4d4c12
Erin Mutchler <mutchler.erin@gmail.com>
2022-12-13 16:16:43 -0800

    Added store

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,28 +1,9 @@
 <template>
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Login</title>
-    </head>
-    <body>
-      <div class="block1">
-      <div class="bounce">
+  <div class="block1">
+    <div class="bounce">
       <img src="https://i.ibb.co/SJFdSg2/html-css-roboi.png">
-      </div>
     </div>
-    
-    
-    
-    
-    
-    
-    
-    
-    
-  
-    </body>
-  </html>
+  </div>
 </template>
 
 <script>
@@ -33,17 +14,17 @@ export default {
 
 <style scoped>
 .block1 {
-background: none;
-height: 100%;
-width: 40%;
-text-align: center;
+  background: none;
+  height: 100%;
+  width: 40%;
+  text-align: center;
 }
 
 .bounce {
-display: flex;
-text-align: center;
-justify-content: center;
-position: relative;
-top: 600px;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  position: relative;
+  top: 600px;
 }
 </style>
